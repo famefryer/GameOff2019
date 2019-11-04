e9d5805ee064bfa156cde3971cea77b055bb87da
Triwith Mutitakul <triwith.m@ku.th>
2019-11-05 00:36:47 +0200

    Create base ninjutsu class

---
+++ Assets/Scripts/Player/Ninjutsu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ninjutsu
+{
+
+    private string jutsuName;
+
+    public Ninjutsu(string name)
+    {
+        jutsuName = name;
+    }
+
+    public string getName()
+    {
+        return jutsuName;
+    }
+
+}
