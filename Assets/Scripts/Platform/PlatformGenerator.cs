b9d2b1e8021d596c8cb831cc35a72e07cd994947
Triwith Mutitakul <triwith.m@ku.th>
2019-11-05 00:24:53 +0200

    Complete simple platform generator

--- Assets/Scripts/Platform/PlatformGenerator.cs
+++ Assets/Scripts/Platform/PlatformGenerator.cs
@@ -7,19 +7,34 @@ public class PlatformGenerator : MonoBehaviour
 
     public GameObject platform;
     public Transform generationPoint;
-    public float distanceBetween;
+    public float minDistance;
+    public float maxDistance;
 
     private float platformWidth;
+    private float currentDistance;
+    private bool isFirstTime;
+    private GameObject previousPlatform;
 
     // Start is called before the first frame update
     void Start()
     {
-       
+        isFirstTime = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Instantiate(platform, generationPoint.position, generationPoint.rotation);
+        if (isFirstTime)
+        {
+            isFirstTime = false;
+            previousPlatform = Instantiate(platform, generationPoint.position, generationPoint.rotation);
+            currentDistance = Random.Range(minDistance, maxDistance);
+        }
+
+        if (Mathf.Abs(previousPlatform.transform.position.x - transform.position.x) > currentDistance)
+        {
+            previousPlatform = Instantiate(platform, generationPoint.position, generationPoint.rotation);
+            currentDistance = Random.Range(minDistance, maxDistance);
+        }
     }
 }
