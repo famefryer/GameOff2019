b6040e653813c7a2b7bf7f8400a12adb8dc9d1a3
Triwith Mutitakul <triwith.m@ku.th>
2019-12-01 14:28:28 +0200

    COmplete score UI

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    [SerializeField]
+    private Text scoreText;
+
+    private void Update()
+    {
+        UpdateValue(GameManager.Instance.currentScore);
+    }
+    public void UpdateValue(int value)
+    {
+        string[] texts = scoreText.text.Split(':');
+        scoreText.text = texts[0] + ": " + value;
+    }
+
+}
